1d77ef6b8aa7c5373696653af96d759bb769944b
PondBiBi <pondmaserbibi@gmail.com>
2025-12-19 23:33:34 +0700

    your message

--- server.js
+++ server.js
@@ -10,8 +10,11 @@ const cors = require('cors')
 // middleware
 app.use(morgan('dev'))
 app.use(express.json({ limit: '20mb' }))
-app.use(cors())
 
+app.use(cors({
+  origin: '*',  // อนุญาตทุก origin
+  credentials: false
+}))
 
 
 readdirSync('./routes')
